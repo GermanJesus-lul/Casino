4ba8f961b5a87ace9f69faa1ca7ecd6ff7270676
Julius <julius.goeler@gmail.com>
2024-02-15 15:08:07 +0100

    force https

--- flask_app.py
+++ flask_app.py
@@ -68,6 +68,14 @@ def userid_from_token(token):
         return None
 
 
+@app.before_request
+def before_request():
+    if not request.is_secure:
+        url = request.url.replace('http://', 'https://', 1)
+        code = 301
+        return redirect(url, code=code)
+
+
 @app.route("/")
 def home():
     return render_template("home.html")
