72675d4283ab87d7988313ab3adf4ccb5c61d3ca
Julius <julius.goeler@gmail.com>
2024-05-22 11:25:35 +0200

    fixed blueprints

--- blueprints/home.py
+++ blueprints/home.py
@@ -5,7 +5,7 @@ home_blueprint = Blueprint('home', __name__)
 
 
 @home_blueprint.route("/")
-def home():
+def homepage():
     if request.method == "GET":
         token = request.cookies.get('token')
         if not token:
