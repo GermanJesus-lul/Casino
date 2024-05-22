d19863844ee6033be0e15203e087a27420ab7616
Julius <julius.goeler@gmail.com>
2024-05-22 15:37:34 +0200

    always redirect to /login if not logged in

--- flask_app.py
+++ flask_app.py
@@ -22,8 +22,10 @@ def before_request():
     if not request.is_secure:
         url = request.url.replace('http://', 'https://', 1)
         return redirect(url, code=301)
+
     token = request.cookies.get('token')
-    if token:
-        user_id = userid_from_token(token)
-        if not user_id:
-            return redirect(url_for('user_administration.login'))
+    if not token:
+        return redirect(url_for('user_administration.login'))
+    user_id = userid_from_token(token)
+    if not user_id:
+        return redirect(url_for('user_administration.login'))
