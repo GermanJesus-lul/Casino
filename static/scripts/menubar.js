ffe0504c8a93699bcd6762fe3d93892a4b6d21ba
Julius <julius.goeler@gmail.com>
2024-05-23 09:31:53 +0200

    update menubar data every 5 seconds

--- static/scripts/menubar.js
+++ static/scripts/menubar.js
@@ -1,14 +1,19 @@
-if (document.cookie.indexOf('token=') >= 0) {
-    fetch("/userdata")
-    .then(async function (response) {
-        let responseJson = await response.json();
-        document.getElementById("balance").innerText = responseJson["balance"];
-        document.getElementById("account-link").href = "/account";
-        document.getElementById("account-link").innerText = responseJson["username"];
-    })
-} else {
-    document.getElementById("balance-div").remove();
+function updateUserdata() {
+    if (document.cookie.indexOf('token=') >= 0) {
+        fetch("/userdata")
+        .then(async function (response) {
+            let responseJson = await response.json();
+            document.getElementById("balance").innerText = responseJson["balance"];
+            document.getElementById("account-link").href = "/account";
+            document.getElementById("account-link").innerText = responseJson["username"];
+        })
+    } else {
+        document.getElementById("balance-div").remove();
 
-    document.getElementById("account-link").href = "/login";
-    document.getElementById("account-link").innerText = "Login";
+        document.getElementById("account-link").href = "/login";
+        document.getElementById("account-link").innerText = "Login";
+    }
 }
+
+updateUserdata()
+setInterval(updateUserdata, 5000);
