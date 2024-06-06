1b4dfb479144e68af33e284c7c439b14aa89943e
Julius <julius.goeler@gmail.com>
2024-06-06 12:19:55 +0200

    fixing bug

--- flask_app.py
+++ flask_app.py
@@ -6,13 +6,11 @@ from blueprints.autodeployment import autodeployment_blueprint
 from blueprints.home import home_blueprint
 from blueprints.user_administration import user_administration_blueprint
 from blueprints.coinflip import coinflip_blueprint
-from blueprints.account import account_blueprint
 
 app = Flask(__name__)
 app.register_blueprint(autodeployment_blueprint)
 app.register_blueprint(home_blueprint)
 app.register_blueprint(user_administration_blueprint)
-app.register_blueprint(account_blueprint)
 
 # games
 app.register_blueprint(coinflip_blueprint, url_prefix='/coinflip')
