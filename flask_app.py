9e6594182f38d7abc716e6e49e3637f48d30d5d5
Julius <julius.goeler@gmail.com>
2024-06-06 12:26:46 +0200

    fixing bug

--- flask_app.py
+++ flask_app.py
@@ -5,7 +5,7 @@ from helper_functions.user_administration import userid_from_token
 from blueprints.autodeployment import autodeployment_blueprint
 from blueprints.home import home_blueprint
 from blueprints.user_administration import user_administration_blueprint
-#from blueprints.coinflip import coinflip_blueprint
+from blueprints.coinflip import coinflip_blueprint
 from blueprints.account import account_blueprint
 
 app = Flask(__name__)
@@ -15,7 +15,7 @@ app.register_blueprint(user_administration_blueprint)
 app.register_blueprint(account_blueprint)
 
 # games
-#app.register_blueprint(coinflip_blueprint, url_prefix='/coinflip')
+app.register_blueprint(coinflip_blueprint, url_prefix='/coinflip')
 
 
 @app.before_request
