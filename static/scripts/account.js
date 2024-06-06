fa252ec9ca27e294094ca3165a6797f8455e410e
Julius <julius.goeler@gmail.com>
2024-06-06 21:24:21 +0200

    trying to fix cookie not actually being deleted

--- static/scripts/account.js
+++ static/scripts/account.js
@@ -2,6 +2,7 @@ let logoutButton = document.getElementById('logout');
 
 function logout() {
     document.cookie = "token= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+    window.location.href = "/";
 }
 
 logoutButton.addEventListener('click', logout);
