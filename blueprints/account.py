eb8025bebc5755305b2641401380aeaf68e9145b
Julius <julius.goeler@gmail.com>
2024-06-06 12:11:20 +0200

    added logout link to account page

--- blueprints/account.py
+++ blueprints/account.py
@@ -1,8 +1,6 @@
 from flask import Blueprint, request, render_template
 
-from helper_functions.user_administration import userid_from_token, userdata_from_id, update_balance, played_game
-
-account_blueprint = Blueprint('coinflip', __name__)
+account_blueprint = Blueprint('account', __name__)
 
 
 @account_blueprint.route('/', methods=["GET", "POST"])
