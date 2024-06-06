348902f763957bd70f6c83dcd49c6d1ec2693402
Julius <julius.goeler@gmail.com>
2024-06-06 09:38:43 +0200

    updated database access flow

--- helper_functions/mysqlClass.py
+++ helper_functions/mysqlClass.py
@@ -1,12 +1,15 @@
 import mysql.connector
+import os
 
+DBUSER = os.getenv('DBUSER')
+DBPASS = os.getenv('DBPASS')
 
 class MySQL:
     def __init__(self, sql_type):
         self.sql_type = sql_type
 
     def __enter__(self):
-        self.con = mysql.connector.connect(user='Betonblock', password='somepasswordformysql',
+        self.con = mysql.connector.connect(user=DBUSER, password=DBPASS,
                                            host='Betonblock.mysql.pythonanywhere-services.com',
                                            database='Betonblock$casino')
         self.cur = self.con.cursor(buffered=True)
