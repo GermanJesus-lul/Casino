5e5ced9f7af3673544e838f62ffdfb10851e21f1
Julius <julius.goeler@gmail.com>
2024-06-06 12:44:11 +0200

    fixing bug

--- blueprints/user_administration.py
+++ blueprints/user_administration.py
@@ -42,7 +42,7 @@ def register():
 @user_administration_blueprint.route("/logout", methods=["GET"])
 def logout():
     resp = make_response(redirect('/'))
-    resp.set_cookie('token', '', expires=0)
+    resp.delete_cookie('token')
     return resp
 
 
