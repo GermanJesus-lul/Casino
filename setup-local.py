58affc2832623054052af0da53288cdfa4866cc8
Julius <julius.goeler@gmail.com>
2024-06-11 09:37:14 +0200

    added mysql database details to config.json

--- setup-local.py
+++ setup-local.py
@@ -15,10 +15,11 @@ con.commit()
 con.close()
 
 
-config_json = {
-    'local': True,
-    'db_path': db_path
-}
+with open('config.json', 'r') as f:
+    config_json = json.load(f)
+
+config_json["local"] = True
+config_json["db_path"] = db_path
 
 with open('config.json', 'w') as f:
     json.dump(config_json, f)
--- setup-remote.py
+++ setup-remote.py
@@ -1,8 +1,9 @@
 import json
 
-config_json = {
-    'local': False
-}
+with open('config.json', 'r') as f:
+    config_json = json.load(f)
+
+config_json["local"] = False
 
 with open('config.json', 'w') as f:
     json.dump(config_json, f)
