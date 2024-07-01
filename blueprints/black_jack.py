2c9a6aa8398f6d361f2d159b8d53cdbd7834c313
Alina Boess <156015430+AlinaBoess@users.noreply.github.com>
2024-07-01 09:30:03 +0200

    Adding JavaScript and Phyton files for BlackJack. There are only the basics in the files.

--- blueprints/black_jack.py
+++ blueprints/black_jack.py
@@ -2,6 +2,8 @@ import random
 
 from flask import Blueprint, request, render_template
 
+from flask import jsonify
+
 from helper_functions.user_administration import userid_from_token, userdata_from_id, update_balance, played_game
 
 black_jack_blueprint = Blueprint('black_jack', __name__)
@@ -17,3 +19,32 @@ def play():
 
     user_id = userid_from_token(request.cookies.get('token'))
     user_data = userdata_from_id(user_id)
+
+
+
+@black_jack_blueprint.route('/deal', methods=["POST"])
+def deal():
+    # This function will handle the initial dealing of cards
+    # Generate two random cards for the dealer and the player
+    # Remember to check if the cards are aces
+    # Return the cards as a response
+    pass  # Placeholder code
+
+@black_jack_blueprint.route('/hit', methods=["POST"])
+def hit():
+    # This function will handle the player's hit action
+    # Generate a random card
+    # Check if the card is an ace
+    # Add the card to the player's hand
+    # Calculate the player's new sum
+    # Check if the player has busted
+    # Return the new card and the player's new sum as a response
+    pass  # Placeholder code
+@black_jack_blueprint.route('/stay', methods=["POST"])
+def stay():
+    # This function will handle the player's stay action
+    # The dealer should continue to draw cards until their sum is 17 or higher
+    # Determine the winner of the game
+    # Update the player's balance based on the game result
+    # Return the result of the game as a response
+    pass  # Placeholder code
--- static/scripts/black_jack.js
+++ static/scripts/black_jack.js
@@ -1,5 +1,16 @@
 document.getElementById("hit").addEventListener("click", hitFunction);
 document.getElementById("stay").addEventListener("click", stayFunction);
+var dealerSum = 0;
+var yourSum = 0;
+
+var dealerAceCount = 0;
+var yourAceCount = 0;
+
+var hidden;
+var deck;
+
+var canHit = true;
+
 
 async function hitFunction() {}
 async function stayFunction() {}
