01c8669027970655b248e33bd46efd1c6e193911
Alina Boess <156015430+AlinaBoess@users.noreply.github.com>
2024-07-04 12:01:00 +0200

    Adding function that a little BlackJack is running. Not possible to make stakes right now.

--- blueprints/black_jack.py
+++ blueprints/black_jack.py
@@ -1,10 +1,42 @@
 import random
 
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, jsonify
 
 from helper_functions.user_administration import userid_from_token, userdata_from_id, update_balance, played_game
 
 black_jack_blueprint = Blueprint('black_jack', __name__)
+
+
+deck = []
+values = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king']
+types = ['clubs', 'diamonds', 'hearts', 'spades']
+
+
+def build_deck():
+    global deck
+    deck = [f"{t}_{v}" for t in types for v in values]
+    random.shuffle(deck)
+
+
+def get_value(card):
+    value = card.split('_')[1]
+    if value == 'ace':
+        return 11
+    elif value in ['jack', 'queen', 'king']:
+        return 10
+    else:
+        return int(value)
+
+
+def check_ace(card):
+    return 1 if card.split('_')[1] == 'ace' else 0
+
+
+def reduce_ace(player_sum, player_ace_count):
+    while player_sum > 21 and player_ace_count > 0:
+        player_sum -= 10
+        player_ace_count -= 1
+    return player_sum
 
 
 @black_jack_blueprint.route('/')
@@ -23,30 +55,46 @@ def play():
 
 @black_jack_blueprint.route('/deal', methods=["POST"])
 def deal():
-    # This function will handle the initial dealing of cards
-    # Generate two random cards for the dealer and the player
-    # Remember to check if the cards are aces
-    # Return the cards as a response
-    pass  # Placeholder code
+    build_deck()
+    player_cards = [deck.pop(), deck.pop()]
+    dealer_cards = [deck.pop(), deck.pop()]
+    player_sum = sum(get_value(card) for card in player_cards)
+    dealer_sum = sum(get_value(card) for card in dealer_cards)
+    player_ace_count = sum(check_ace(card) for card in player_cards)
+    dealer_ace_count = sum(check_ace(card) for card in dealer_cards)
+    player_sum = reduce_ace(player_sum, player_ace_count)
+    dealer_sum = reduce_ace(dealer_sum, dealer_ace_count)
+    response = {
+        "player_cards": player_cards,
+        "dealer_cards": [dealer_cards[0],"back"],
+        "player_sum": player_sum,
+        "dealer_sum": get_value(dealer_cards[0]),
+        "player_ace_count": player_ace_count,
+        "dealer_ace_count": dealer_ace_count
+    }
+    return jsonify(response)
 
 
 @black_jack_blueprint.route('/hit', methods=["POST"])
 def hit():
-    # This function will handle the player's hit action
-    # Generate a random card
-    # Check if the card is an ace
-    # Add the card to the player's hand
-    # Calculate the player's new sum
-    # Check if the player has busted
-    # Return the new card and the player's new sum as a response
-    pass  # Placeholder code
+    card = deck.pop()
+    card_value = get_value(card)
+    card_ace = check_ace(card)
+    return jsonify({"card": card, "value": card_value, "ace": card_ace})
 
 
 @black_jack_blueprint.route('/stay', methods=["POST"])
 def stay():
-    # This function will handle the player's stay action
-    # The dealer should continue to draw cards until their sum is 17 or higher
-    # Determine the winner of the game
-    # Update the player's balance based on the game result
-    # Return the result of the game as a response
-    pass  # Placeholder code
+    data = request.json
+    dealer_sum = data['dealer_sum']
+    dealer_ace_count = data['dealer_ace_count']
+    dealer_cards = data['dealer_cards']
+
+    while dealer_sum < 17:
+        card = deck.pop()
+        dealer_sum += get_value(card)
+        dealer_ace_count += check_ace(card)
+        dealer_sum = reduce_ace(dealer_sum, dealer_ace_count)
+        dealer_cards.append(card)
+
+    return jsonify({"dealer_sum": dealer_sum, "dealer_cards": dealer_cards})
