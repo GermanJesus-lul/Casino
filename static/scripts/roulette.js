18ec36bcda0a4412e389c02aeeadce07f1822ceb
Jaro1501 <155685773+Jaro1501@users.noreply.github.com>
2024-07-11 02:14:35 +0200

    Better animation + ball

--- static/scripts/roulette.js
+++ static/scripts/roulette.js
@@ -2,6 +2,7 @@ let spinButton = document.getElementById("spin");
 let redButton = document.getElementById("red");
 let blackButton = document.getElementById("black");
 let wheel = document.getElementById("wheel_img");
+let ball = document.getElementById("ball_img");
 
 const numberToPositionIndex = [0, 26, 3, 35, 12, 28, 7, 29, 18, 22, 9, 31, 14, 20, 1, 33, 16, 24, 5, 10, 23, 8, 30, 11, 36, 13, 27, 6, 37, 34, 17, 25, 2, 21, 4, 19, 15, 32]
 
