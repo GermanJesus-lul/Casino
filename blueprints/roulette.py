8dc8525817235692d2c3e57cfe768053bd0f3874
Jaro1501 <155685773+Jaro1501@users.noreply.github.com>
2024-07-14 03:09:10 +0200

    finish

--- blueprints/roulette.py
+++ blueprints/roulette.py
@@ -4,11 +4,15 @@ from flask import Blueprint, request, render_template
 
 from helper_functions.user_administration import userid_from_token, userdata_from_id, update_balance
 from helper_functions.stats import played_game
+
 roulette_blueprint = Blueprint('roulette', __name__)
 
 red = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
 black = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 29, 31, 33, 35]
 green = [0]
+
+even = [i for i in range(0, 37) if i % 2 == 0]
+odd = [i for i in range(0, 37) if i % 2 != 0]
 
 
 @roulette_blueprint.route('/')
@@ -26,11 +30,27 @@ def spin():
 
     if int(user_data['balance']) >= int(content['bet']):
         result = random.randint(0, 37)
-        if content['choice'] == "red" and result in red or content['choice'] == "black" and result in black or \
-                content['choice'] == "green" and result in green:
-            bet_value = int(content['bet'])
+        if content['type'] == "number":
+            if int(content['number']) is None:
+                return "invalid number"
+            elif int(content['number']) == result:
+                bet_value = 36 * int(content['bet'])
+            else:
+                bet_value = -int(content['bet'])
+        elif content['choice'] is None:
+            return "invalid choice"
+        elif content['type'] == "evenodd":
+            if content['choice'] == "even" and result in even or content['choice'] == "odd" and result in odd:
+                bet_value = int(content['bet'])
+            else:
+                bet_value = -int(content['bet'])
+        elif content['type'] == "color":
+            if content['choice'] == "red" and result in red or content['choice'] == "black" and result in black:
+                bet_value = int(content['bet'])
+            else:
+                bet_value = -int(content['bet'])
         else:
-            bet_value = -int(content['bet'])
+            return "invalid choice"
 
         update_balance(user_id, bet_value)
         played_game(user_id, bet_value, "roulette", double_field=result)
--- static/scripts/roulette.js
+++ static/scripts/roulette.js
@@ -1,6 +1,4 @@
 let spinButton = document.getElementById("spin");
-let redButton = document.getElementById("red");
-let blackButton = document.getElementById("black");
 let wheel = document.getElementById("wheel_img");
 let ball = document.getElementById("ball_img");
 
@@ -9,35 +7,75 @@ const numberToPositionIndex = [0, 26, 3, 35, 12, 28, 7, 29, 18, 22, 9, 31, 14, 2
 let choice = "red"
 
 async function spin() {
+    spinButton.disabled = true;
+    wheel.style.setProperty("--start-deg", "0deg");
+    wheel.style.setProperty("--target-wait-deg", "360deg");
     const timeoutPromise = new Promise(resolve => setTimeout(resolve, 2000));
     const fetchPromise = fetch("/roulette/spin", {
         method: "POST",
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
             "bet": document.getElementById("bet").value,
-            "choice": choice
+            "type": document.getElementById("bet-type").value,
+            "choice": document.querySelector('input[name="choice"]:checked') ? document.querySelector('input[name="choice"]:checked').value : null,
+            "number": document.getElementById("number") ? document.getElementById("number").value : null
         })
+        // "choice": choice
     }).then(response => response.text());
 
     Promise.all([fetchPromise, timeoutPromise]).then(async (values) => {
         const positionIndex = parseInt(values[0]);
         const result = numberToPositionIndex[positionIndex];
         const targetDeg = result * 9.473684210526316;
-        wheel.style.setProperty("--target-deg", `${targetDeg + (3*360)}deg`);
+        wheel.style.setProperty("--target-deg", `${targetDeg + (3 * 360)}deg`);
         wheel.classList.add("spinning");
         wheel.addEventListener("animationend", function () {
             wheel.style.transform = `rotate(${result * 9.473684210526316}deg)`;
             wheel.classList.remove("spinning");
             wheel.style.setProperty("--start-deg", `${targetDeg}deg`);
-        } , {once: true});
-        updateUserdata();
+            wheel.style.setProperty("--target-wait-deg", `${targetDeg + 360}deg`);
+            updateUserdata();
+            spinButton.disabled = false;
+        }, {once: true});
     });
 }
 
-redButton.addEventListener("click", function () {
-    choice = "red"
-})
-blackButton.addEventListener("click", function () {
-    choice = "black"
-})
 spinButton.addEventListener('click', spin);
+
+function updateInputContainer() {
+    var selectedType = document.getElementById('bet-type').value;
+    var inputContainer = document.getElementById('input-container');
+
+    inputContainer.innerHTML = ''; // Vorherige Eingaben löschen
+
+    if (selectedType === 'color') {
+        inputContainer.innerHTML = `
+            <label class="choice-radio-label"><input class="choice-radio-button" type="radio" value="red" id="red" name="choice" checked="checked"> Red</label>
+            <label class="choice-radio-label"><input class="choice-radio-button" type="radio" value="black" id="black" name="choice"> Black</label>
+        `;
+    } else if (selectedType === 'evenodd') {
+        inputContainer.innerHTML = `
+            <label class="choice-radio-label"><input class="choice-radio-button" type="radio" value="even" id="even" name="choice" checked="checked"> Even</label>
+            <label class="choice-radio-label"><input class="choice-radio-button" type="radio" value="odd" id="odd" name="choice"> Odd</label>
+        `;
+    } else if (selectedType === 'number') {
+
+        inputContainer.innerHTML = `
+            <div style="display: inline">
+                <button type="button" class="input-number-button"
+                        onclick="this.parentNode.querySelector('[type=number]').stepDown();">
+                    -
+                </button>
+                <input id="number" type="number" min="0" max="36" step="1" value="0"/>
+                <button type="button" class="input-number-button"
+                        onclick="this.parentNode.querySelector('[type=number]').stepUp();">
+                    +
+                </button>
+            </div>
+        `;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', updateInputContainer);
+
+document.getElementById('bet-type').addEventListener('change', updateInputContainer);
