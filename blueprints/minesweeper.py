2341794b9fab3d4612972c09d7e7177cca28ad73
Luca <luca@die5grimms.de>
2024-08-08 13:02:32 +0200

    implemented place bet and cashout functionality

--- blueprints/minesweeper.py
+++ blueprints/minesweeper.py
@@ -1,6 +1,6 @@
 import random
 
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, jsonify
 
 from helper_functions.user_administration import userid_from_token, userdata_from_id, update_balance
 from helper_functions.stats import played_game
@@ -12,6 +12,7 @@ user_bet = 0
 user_cash_out_val = 0
 mines_count = 0
 user_guesses_count = 0
+game_running = False
 
 
 @minesweeper_blueprint.route('/')
@@ -22,10 +23,15 @@ def minesweeper_home():
 
 @minesweeper_blueprint.route('/newGame', methods=["POST"])
 def create_minesweeper():
+    global user_bet
+    if user_bet != 0:
+        return "theres already a game running"
     content = request.json
 
     count = content["count"]
     bet = content["bet"]
+
+    print("Count: " + str(count) + " Bet: " + str(bet))
 
     user_id = userid_from_token(request.cookies.get('token'))
     user_data = userdata_from_id(user_id)
@@ -34,7 +40,8 @@ def create_minesweeper():
 
         update_balance(user_id, bet * -1)  # subtracts bet value from user balance
 
-        global user_bet, user_cash_out_val, mines_count, user_guesses_count, mineField
+        global user_cash_out_val, mines_count, user_guesses_count, mineField, game_running
+        game_running = True
         user_bet = bet  # set value of global variable 'user_bet' to the bet amount
         user_cash_out_val = bet
         mines_count = count  # sets value of global 'mines_count' to the mines count
@@ -65,21 +72,25 @@ def get_field_value():
     global user_cash_out_val, mines_count, user_guesses_count, mineField
     if mineField[pos]:
         user_cash_out_val = 0
-        return 0, 0
+        return jsonify(0, 0)
     else:
         multiplier = (mines_count / (25 - user_guesses_count)) + 1
         user_guesses_count += 1
         user_cash_out_val *= multiplier
-        return multiplier, user_cash_out_val
+        return jsonify(multiplier, user_cash_out_val)
 
 
-@minesweeper_blueprint.route('/cashOut', methods=["GET"])
+@minesweeper_blueprint.route('/cashOut', methods=["POST"])
 def cash_out_minesweeper():
     global user_bet, user_cash_out_val, mineField
 
     user_id = userid_from_token(request.cookies.get('token'))
 
     update_balance(user_id, user_cash_out_val)
-    played_game(user_id, user_bet, "minesweeper", text_field=user_cash_out_val)
 
-    return "Cashed out ${:.2f}".format(user_cash_out_val)
+    ret_str = "Cashed out {:.2f}".format(user_cash_out_val)
+
+    user_cash_out_val = 0
+    user_bet = 0
+
+    return ret_str
--- static/scripts/minesweeperFrontEnd.js
+++ static/scripts/minesweeperFrontEnd.js
@@ -1,8 +1,65 @@
 // Referenz zur grid-container-Div erhalten
 const gridContainer = document.getElementById('grid-container');
+const betButton  = document.getElementById('betButton');
+const cashOutButton = document.getElementById('cashOutButton');
+const betValue = document.getElementById('betValue');
+const mineCount = document.getElementById('mineCount');
 
-function buttonClicked(number) {
-    console.log("Button " + number + " wurde gedrückt");
+betButton.addEventListener('click', placeBet)
+cashOutButton.addEventListener('click', cashOut)
+
+
+async function placeBet() {
+    fetch('/minesweeper/newGame', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            "count": parseInt(mineCount.value),
+            "bet": parseInt(betValue.value)
+        })
+    })
+    .then(async function (response) {
+        let result = await response.text();
+        if (result === "minesweeper created") {
+            // disable bet-area and show the cash-out-area
+            alert("Created Game");
+            document.getElementById('cashOutValue').innerText = betValue.value;
+        }
+        else alert(result);
+    })
+    .then(async function (response) {
+        updateUserdata();
+    })
+}
+
+async function cashOut() {
+    fetch('/minesweeper/cashOut', {
+        method: 'POST'})
+    .then(async function (response) {
+        let result = await response.text();
+        alert(result);
+    })
+    .then(async function (response) {
+        updateUserdata();
+        document.getElementById('cashOutValue').innerText = '0';
+    })
+}
+
+async function buttonClicked(number) {
+    console.log(`clicked ${number}`);
+    /*
+    fetch('minesweeper/try', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            "pos": number
+        })
+    }).then(response => response.json())
+        .then(async function (response) {
+            response[0]
+        })
+    })
+     */
 }
 
 // Buttons dynamisch erzeugen und Event Listener hinzufügen
